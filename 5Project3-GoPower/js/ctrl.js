56e242875d4bbc66c52bf4c0f73805440c379257
jacktea88 <jacktea@gmail.com>
2026-01-12 15:38:51 +0800

    禁止eydowm功能

--- 5Project3-GoPower/js/ctrl.js
+++ 5Project3-GoPower/js/ctrl.js
@@ -1,15 +1,43 @@
 
-	$(function(){
-		document.onkeydown=function(e){
-			// alert(e.keyCode)
-			if(e.keyCode===17 ||e.keyCode===85||e.keyCode===123){
-				return false;
-			}else{
-				return true;
-			}
-		}
-		$("body").on('contextmenu',function(){
-			$("#warning").modal("show")
-			return false;
-		})
-	})
+	// 禁止右鍵
+        document.oncontextmenu = function(){
+            // alert("禁止使用瀏覽器快捷鍵");
+            showWarning();
+            return false;
+        }
+
+        function showWarning(){
+            let warning = new bootstrap.Modal(document.querySelector("#warning"));
+            warning.show();
+        }
+
+        // 禁止keydown
+        document.onkeydown = function (e) {
+            // alert(e.keyCode)
+            if (e.keyCode == 123 || e.keyCode == 17 || e.keyCode == 85) {
+            //    alert("禁止使用按鍵");
+               showWarning();
+               return false; 
+            }
+            else{
+                return true;
+            }
+            
+
+        }
+	
+	
+	// $(function(){
+	// 	document.onkeydown=function(e){
+	// 		// alert(e.keyCode)
+	// 		if(e.keyCode===17 ||e.keyCode===85||e.keyCode===123){
+	// 			return false;
+	// 		}else{
+	// 			return true;
+	// 		}
+	// 	}
+	// 	$("body").on('contextmenu',function(){
+	// 		$("#warning").modal("show")
+	// 		return false;
+	// 	})
+	// })
